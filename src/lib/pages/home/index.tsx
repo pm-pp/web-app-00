cda221d5423a7a98d9340ca23829b9730bed1ce1
Priyanga Gamage <6237841+p-gamage@users.noreply.github.com>
2022-04-26 11:02:46 +0100

    Update index.tsx

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -2,7 +2,6 @@ import { Grid } from "@chakra-ui/react";
 
 import CTASection from "./components/CTASection";
 import SomeImage from "./components/SomeImage";
-import SomeSection from "./components/SomeSection";
 import SomeText from "./components/SomeText";
 
 const Home = () => {
